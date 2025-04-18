4eeff79bcb9e9c09089ab0e0dca206176309a37f
Arlox <itsnotgamez11@gmail.com>
2025-04-18 16:18:45 +0930

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function historyback() {
+
+}
+function historyfwd() {
+
+}
+function browserhome() {
+
+}
+function browserrefresh() {
+
+}
+document.getElementById("screenw").innerHTML =
+"Screen Width: " + screen.width;
