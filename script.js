bb20037647aec0cec85c9d5674c5d67c668022f2
Arlox <itsnotgamez11@gmail.com>
2025-04-22 09:51:48 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function historyback() {
 
 }
+
 function historyfwd() {
 
 }
+
 function browserhome() {
 
 }
+
 function browserrefresh() {
 
 }
+
 document.getElementById("screenw").innerHTML =
-"Screen Width: " + screen.width;
+ "Screen Width: " + screen.width;
+
+document.getElementById("screenh").innerHtML =
+ "Screen Height: " + screen.height;
